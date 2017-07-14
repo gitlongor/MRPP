a1a582bfdfe048b0ff04041a0de71cc5f12d70f9
Long Qu <rtist@qq.com>
2017-07-14 19:29:18 +0800

    added more plots to bw.smoothp

--- R/bw.R
+++ R/bw.R
@@ -52,6 +52,17 @@ function(y, permutedTrt, r=seq_len(NCOL(y)), bw = NULL,
 	nms = setdiff(c(names(formals(mrpp.test.dist)), names(formals(mrpp))), '...')
 	idx=names(lst)%in%nms
 	mrpp = do.call('mrpp.test.dist', c(lst[idx],list(method='permutation')))
+	
+	plot.expr=expression({
+		hist(mrpp$all.statistics, breaks='FD', freq=FALSE, main='Density of MRPP statistics', ylab='Density', xlab='MRPP z-statistics')
+		pdf.final=dkde(mrpp$all.statistics, ans, kernel)
+		marg = diff(range(mrpp$all.statistics))/5
+		curve(pdf.final(x), min(mrpp$all.statistics)-marg, max(mrpp$all.statistics)+marg, add=TRUE)
+		rug(mrpp$all.statistics)
+		abline(v=mrpp$all.statistics[1L], col='blue', lty=3L)
+		axis(1L, at=mrpp$all.statistics[1L], labels=expression(z[1L]), col='blue', line=1, col.ticks='blue', col.axis='blue')
+		box()
+	})
 	if(method=='kde.mse1'){
 		lst=list(...)
 		lst$x=mrpp$all.statistics
@@ -59,7 +70,9 @@ function(y, permutedTrt, r=seq_len(NCOL(y)), bw = NULL,
 		lst$verbose=verbose
 		nms=names(lst)
 		idx= nms=='' || nms%in%names(formals(bw.mse.pdf.asym))
-		return(do.call('bw.mse.pdf.asym', lst[idx]))
+		ans = do.call('bw.mse.pdf.asym', lst[idx])
+		if(verbose) eval(plot.expr)
+		return(ans)
 	}
 
 	weight.trt = attr(mrpp$parameter, 'weight.trt')
@@ -85,6 +98,7 @@ function(y, permutedTrt, r=seq_len(NCOL(y)), bw = NULL,
 		idx = which.max(ss)
 		ans=bw[idx]
 		if(verbose){
+			dev.new(width=10, height=5, noRStudioGD=TRUE); par(mfrow=1:2)
 			plot(log10(bw), (ss), xlab='bandwidth', ylab='SS of p-value gradients', type='o', axes=FALSE )
 			axis(2)
 			axis(1, at = log10(ans), labels=sprintf('%.1g',ans), col='blue',col.ticks='blue', col.axis='blue')
@@ -92,6 +106,8 @@ function(y, permutedTrt, r=seq_len(NCOL(y)), bw = NULL,
 			axis(1, at=ats, labels=parse(text=paste0('10^',ats))  )
 			abline(v=log10(ans), h=(ss[idx]), col='blue', lty=3)
 			box()
+			
+			eval(plot.expr)
 		}
 		if(idx==1L || idx==length(bw))warning('optimal bandwidth occurs at the boundary')
 		return(  exp(mean(log(ans)))  )
@@ -135,6 +151,9 @@ function(y, permutedTrt, r=seq_len(NCOL(y)), bw = NULL,
 	min.sse=sses[idx]
 	
 	if(verbose){
+		dev.new(width=10, height=5, noRStudioGD=TRUE)
+		par(mfrow=1:2)
+
 		plot(log10(bw), sses, xlab='bandwidth', ylab='SS of approx errors', type='o', main='', axes=FALSE)
 		title(main=switch(method, drop1='Backward Difference', add1='Forward Difference', keep1='Keep 1 Variable', sym1='Central Difference', dropadd1='Total From Backward and Forward Difference', dropaddsym1='Total From Backward, Forward and Central Difference'))
 		axis(3, at = log10(ans), labels=sprintf('%.1g',ans), col='red',col.ticks='red',col.axis='red')
@@ -145,6 +164,18 @@ function(y, permutedTrt, r=seq_len(NCOL(y)), bw = NULL,
 		
 		axis(1, at = log10(ans), labels=sprintf('%.1g',ans), col='blue',col.ticks='blue', col.axis='blue')
 		abline(v=log10(ans), col='blue', lty=3)
+
+			eval(plot.expr)
+	if(FALSE){
+		hist(mrpp$all.statistics, breaks='FD', freq=FALSE, main='Density of MRPP statistics', ylab='Density', xlab='MRPP z-statistics')
+		pdf.final=dkde(mrpp$all.statistics, ans, kernel)
+		marg = diff(range(mrpp$all.statistics))/5
+		curve(pdf.final(x), min(mrpp$all.statistics)-marg, max(mrpp$all.statistics)+marg, add=TRUE)
+		rug(mrpp$all.statistics)
+		abline(v=mrpp$all.statistics[1L], col='blue', lty=3L)
+		axis(1L, at=mrpp$all.statistics[1L], labels=expression(z[1L]), col='blue', line=1, col.ticks='blue', col.axis='blue')
+		box()
+	}
 	}
 	ans
 }
